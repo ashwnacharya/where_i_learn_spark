2f36f05afccdfb761692256d128ee6098e6761b8
ashwnacharya <b.ashwin.acharya@gmail.com>
2020-02-19 16:54:52 +0530

    Implements the correct way to use ipython with pyspark.

--- roles/spark/files/spark-paths.sh
+++ roles/spark/files/spark-paths.sh
@@ -3,3 +3,4 @@ export PATH=$PATH:$SPARK_HOME/bin
 export PYSPARK_PYTHON=python3
 export PATH=$PATH:$JAVA_HOME/jre/bin
 export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
+export PYSPARK_DRIVER_PYTHON="ipython"
