553afba17b0b805e9f62c3e3133f69e2f70b037b
ashwnacharya <b.ashwin.acharya@gmail.com>
2020-02-19 16:39:47 +0530

    Using ipython does not work as intended. Reverting to python3 for now.

--- roles/spark/files/spark-paths.sh
+++ roles/spark/files/spark-paths.sh
@@ -1,5 +1,5 @@
 export SPARK_HOME=/opt/spark/spark-2.4.5-bin-hadoop2.7
 export PATH=$PATH:$SPARK_HOME/bin
-export PYSPARK_PYTHON=ipython
+export PYSPARK_PYTHON=python3
 export PATH=$PATH:$JAVA_HOME/jre/bin
 export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
